281a489dea18fd25b3866c2b272fa9c4b16da015
Eric Huang <erichuang676@gmail.com>
2021-10-28 17:23:12 +0300

    make mobile layout for all pages of terms and conditions

--- components/TermsAndConditionsComponents/Player.js
+++ components/TermsAndConditionsComponents/Player.js
@@ -6,18 +6,46 @@ const playerStyles = {
   width: '640px',
   height: '405px'
 }
+const playerStylesTablets = {
+  margin: "0 auto",
+  width: '498px',
+  height: '349px'
+}
 const playerStylesMobile = {
   margin: "0 auto",
   width: '398px',
   height: '249px'
 }
+const playerStylesMiniMobile = {
+  margin: "0 auto",
+  width: '258px',
+  height: '149px'
+}
 
 export const Player = ({url}) => {
   const {height, width} = useWindowDimensions();
 
+  let style = playerStyles;
+
+  if (width > 740) {
+    style = playerStyles;
+  } else if (width > 620) {
+    style = playerStylesTablets;
+  } else if (width > 420) {
+    style = playerStylesMobile;
+  } else if (width <= 420) {
+    style = playerStylesMiniMobile;
+  }
 
 
   return (
-    <ReactPlayer url={url} playing controls light={true} style={playerStylesMobile} />
+    <ReactPlayer
+      url={url}
+      controls={true}
+      width={style.width}
+      height={style.height}
+      // light={true}
+      style={playerStyles}
+    />
   )
 }
