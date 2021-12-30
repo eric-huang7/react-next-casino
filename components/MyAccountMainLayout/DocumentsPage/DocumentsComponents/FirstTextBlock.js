13fe0aa01761d29909c94140126b3011b0dea1f0
Eric Huang <erichuang676@gmail.com>
2021-12-30 17:54:29 +0200

    add additional text for documents page

--- components/MyAccountMainLayout/DocumentsPage/DocumentsComponents/FirstTextBlock.js
+++ components/MyAccountMainLayout/DocumentsPage/DocumentsComponents/FirstTextBlock.js
@@ -1,7 +1,11 @@
 import styles from '../../../../styles/MyAccount/DocumentsPage/DocumentsPage.module.scss';
+import {useState} from "react";
 
 
 export const FirstTextBlock = ({t}) => {
+  const [closeFirstBlock, setCloseFirstBlock] = useState(false);
+  const [closeSecondBlock, setCloseSecondBlock] = useState(false);
+  const [closeThirdBlock, setCloseThirdBlock] = useState(false);
 
   return (
     <>
@@ -9,14 +13,68 @@ export const FirstTextBlock = ({t}) => {
         {t("myAccount.documentsPage.firstTextBlock.textListHeading")}
       </p>
       <ul className={styles.textList}>
-        <li>
-          {t("myAccount.documentsPage.firstTextBlock.proofIdentity")}
+        <li className={closeFirstBlock ? styles.upIndicator : ""}>
+          {t("myAccount.documentsPage.firstTextBlock.proofIdentity")} <span onClick={() => setCloseFirstBlock(!closeFirstBlock)}>{t("myAccount.documentsPage.more")}</span>
+          <ul className={`${styles.textList} ${styles.innerTextList} ${closeFirstBlock ? "" : styles.hideTextList}`}>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofIdentityInnerTextBlock.text1")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofIdentityInnerTextBlock.text2")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofIdentityInnerTextBlock.text3")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofIdentityInnerTextBlock.text4")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofIdentityInnerTextBlock.text5")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofIdentityInnerTextBlock.text6")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofIdentityInnerTextBlock.text7")}
+            </li>
+          </ul>
         </li>
-        <li>
-          {t("myAccount.documentsPage.firstTextBlock.proofAddress")}
+        <li className={closeSecondBlock ? styles.upIndicator : ""}>
+          {t("myAccount.documentsPage.firstTextBlock.proofAddress")} <span onClick={() => setCloseSecondBlock(!closeSecondBlock)}>{t("myAccount.documentsPage.more")}</span>
+          <ul className={`${styles.textList} ${styles.innerTextList} ${closeSecondBlock ? "" : styles.hideTextList}`}>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofAddressInnerTextBlock.text1")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofAddressInnerTextBlock.text2")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofAddressInnerTextBlock.text3")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofAddressInnerTextBlock.text4")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofAddressInnerTextBlock.text5")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofAddressInnerTextBlock.text6")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofAddressInnerTextBlock.text7")}
+            </li>
+          </ul>
         </li>
-        <li>
-          {t("myAccount.documentsPage.firstTextBlock.proofDeposit")}
+        <li className={closeThirdBlock ? styles.upIndicator : ""}>
+          {t("myAccount.documentsPage.firstTextBlock.proofDeposit")} <span onClick={() => setCloseThirdBlock(!closeThirdBlock)}>{t("myAccount.documentsPage.more")}</span>
+          <ul className={`${styles.textList} ${styles.innerTextList} ${closeThirdBlock ? "" : styles.hideTextList}`}>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofDepositInnerTextBlock.text1")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.firstTextBlock.proofDepositInnerTextBlock.text2")}
+            </li>
+          </ul>
         </li>
       </ul>
     </>
--- components/MyAccountMainLayout/DocumentsPage/DocumentsComponents/SecondTextBlock.js
+++ components/MyAccountMainLayout/DocumentsPage/DocumentsComponents/SecondTextBlock.js
@@ -1,7 +1,11 @@
 import styles from '../../../../styles/MyAccount/DocumentsPage/DocumentsPage.module.scss';
+import {useState} from "react";
 
 
 export const SecondTextBlock = ({t}) => {
+  const [closeFirstBlock, setCloseFirstBlock] = useState(false);
+  const [closeSecondBlock, setCloseSecondBlock] = useState(false);
+
 
   return (
     <>
@@ -12,11 +16,45 @@ export const SecondTextBlock = ({t}) => {
         <li>
           {t("myAccount.documentsPage.secondTextBlock.netellerOrSkrill")}
         </li>
-        <li>
-          {t("myAccount.documentsPage.secondTextBlock.creditCard")}
+        <li className={closeFirstBlock ? styles.upIndicator : ""}>
+          {t("myAccount.documentsPage.secondTextBlock.creditCard")} <span onClick={() => setCloseFirstBlock(!closeFirstBlock)}>{t("myAccount.documentsPage.more")}</span>
+          <ul className={`${styles.textList} ${styles.innerTextList} ${closeFirstBlock ? "" : styles.hideTextList}`}>
+            <li>
+              {t("myAccount.documentsPage.secondTextBlock.creditCardInnerTextBlock.text1")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.secondTextBlock.creditCardInnerTextBlock.text2")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.secondTextBlock.creditCardInnerTextBlock.text3")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.secondTextBlock.creditCardInnerTextBlock.text4")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.secondTextBlock.creditCardInnerTextBlock.text5")}
+            </li>
+          </ul>
         </li>
-        <li>
-          {t("myAccount.documentsPage.secondTextBlock.bankWithdrawal")}
+        <li className={closeSecondBlock ? styles.upIndicator : ""}>
+          {t("myAccount.documentsPage.secondTextBlock.bankWithdrawal")} <span onClick={() => setCloseSecondBlock(!closeSecondBlock)}>{t("myAccount.documentsPage.more")}</span>
+          <ul className={`${styles.textList} ${styles.innerTextList} ${closeSecondBlock ? "" : styles.hideTextList}`}>
+            <li>
+              {t("myAccount.documentsPage.secondTextBlock.bankWithdrawalInnerTextBlock.text1")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.secondTextBlock.bankWithdrawalInnerTextBlock.text2")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.secondTextBlock.bankWithdrawalInnerTextBlock.text3")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.secondTextBlock.bankWithdrawalInnerTextBlock.text4")}
+            </li>
+            <li>
+              {t("myAccount.documentsPage.secondTextBlock.bankWithdrawalInnerTextBlock.text5")}
+            </li>
+          </ul>
         </li>
       </ul>
       <p className={`${styles.secondListTextBlock} ${styles.secondTextBlock}`}>
