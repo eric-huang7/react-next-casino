d1a8b39281f9967f3d11c631f538f213fb48d245
Eric Huang <erichuang676@gmail.com>
2022-01-17 14:15:30 +0200

    add different colors for base background in currency switcher

--- components/MainLayout/SelectCurrencyWidget/CurrencySelector/CurrencyItem.js
+++ components/MainLayout/SelectCurrencyWidget/CurrencySelector/CurrencyItem.js
@@ -8,12 +8,46 @@ export const CurrencyItem = ({t, currencyData, currencySelectorHandler}) => {
   const name = currencyData.name;
   const base = currencyData.base;
   let colorBase = "#4fadcf";
+  const baseVariants = {
+    'ETH': "#4fadcf",
+    'EOS': "#002fe8",
+    'NEO': "#50b271",
+    'BNB': "#f0b90b",
+    'XLM': "#b362ff",
+    'MATIC': "#7123af",
+    'ATOM': "#140069",
+    'KLAY': "#3b2727",
+    'TRX': "#f00b0b",
+    'RBTC': "#9f7e38",
+    'ONT': "#0088a2",
+    'WAVES': "#124fff",
+    'HTML': "#001d8f",
+    'CHZ': "#e14040",
+    'TOMO': "#313131",
+    'FTM': "#3a7bfa",
+    'XDC': "#3b447a",
+    'CELO': "#d5ba3b",
+    'STAKE': "#434356",
+    'AVAX': "#e05555",
+    'SOL': "#67498f",
+    'ZIL': "#6fbdba",
+    'HT': "#272657",
+    'WAN': "#4574da",
+    'IOST': "#000000",
+    'LUNA': "#00133f",
+    'ADA': "#112475",
+  }
+
+  // base === "ETH" ? "#4fadcf" : base === "TRX" ? "#f00b0b" : base === "BNB" ? "#f0b90b" : "#ef8a13";
 
   if (base) {
-    colorBase = base === "ETN" ? "#4fadcf" : base === "TRX" ? "#f00b0b" : base === "BSC" ? "#f0b90b" : "#ef8a13";
+    colorBase = !!baseVariants[base] ? baseVariants[base] : "#ef8a13";
   } else {
     colorBase = null;
   }
+
+
+
 
 useEffect(() => {
   function svgSetter () {
