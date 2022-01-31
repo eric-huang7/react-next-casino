51cff46efdebc6bc2401171a42fdb7b587edfe10
Eric Huang <erichuang676@gmail.com>
2022-01-31 18:34:32 +0200

    add functionality for close bonus info window

--- components/BonusInfoComponents/BonusInfoContainer.js
+++ components/BonusInfoComponents/BonusInfoContainer.js
@@ -1,12 +1,29 @@
 import styles from "../../styles/BonusInfoComponent/BonusInfoComponent.module.scss";
-import {useRef} from "react";
+import {useEffect, useRef} from "react";
 import {MainHeading} from "./MainHeading";
 import {MainBonusInfoContainer} from "./MainBonusInfoContainer";
+import {showForgotPasswordPopup} from "../../redux/actions/showPopups";
+import {useDispatch} from "react-redux";
 
 
 export const BonusInfoContainer = () => {
   const bonusInfoRef = useRef();
+  const dispatch = useDispatch();
 
+
+  const handleOutsideClick = (event) => {
+    const path = event.path || (event.composedPath && event.composedPath());
+    if (!path.includes(bonusInfoRef.current)) {
+      // dispatch(showForgotPasswordPopup(false));
+    }
+  };
+  useEffect(() => {
+    document.body.addEventListener("click", handleOutsideClick);
+    return () => {
+      // dispatch(showTournaments(true));
+      document.body.removeEventListener('click', handleOutsideClick);
+    }
+  }, []);
 
   return (
     <div className={`${styles.mainWrapper}`}>
