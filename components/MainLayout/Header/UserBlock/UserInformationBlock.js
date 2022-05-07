8bb05367985dc02838ee136eb9cdef194afe1ef7
Eric Huang <erichuang676@gmail.com>
2022-05-06 21:17:19 -0400

    refs:#1306 Header balance

--- components/MainLayout/Header/UserBlock/UserInformationBlock.js
+++ components/MainLayout/Header/UserBlock/UserInformationBlock.js
@@ -1,10 +1,49 @@
 import styles from '../../../../styles/Header/UserBlock.module.scss'
 import { DesktopMenuContainer } from '../../../DesktopMenuComponents/DesktopMenuContainer'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import {numberTransformer} from "../../../../helpers/numberTransformer";
+import {currencyFinder} from "../../../../helpers/currencyFinder";
+import {showDepositModal} from "../../../../redux/popups/action";
+import {useDispatch} from "react-redux";
+import {useTranslation} from "react-i18next";
+import {HeaderBalance} from "./HeaderBalance";
 
 export const UserInformationBlock = ({ userInfo, userCurrency }) => {
-
+  const { t } = useTranslation('common')
+  const dispatch = useDispatch()
+  const [balance, setBalance] = useState(false)
+  const [balanceData, setBalanceData] = useState(false)
   const [isShowLinksMenu, setIsShowLinksMenu] = useState(false)
+  const [activeCurrency, setActiveCurrency] = useState(false)
+  const [currency, setCurrency] = useState(false)
+
+  useEffect(() => {
+    if (userCurrency.currency && userInfo.balance) {
+      let balanceData = userInfo?.balance?.balances.filter((el) => !!Number(el.is_default))
+
+      if (balanceData.length === 0) {
+        if (userInfo?.balance?.balances?.length > 0) {
+          balanceData = userInfo?.balance?.balances
+        }
+      }
+      setBalanceData(balanceData)
+      let amount = ''
+
+      try {
+        amount = numberTransformer(`${balanceData[0].current_balance}`)
+      } catch (e) {
+        amount = ''
+      }
+
+      let balance = balanceData.length === 0 ? '0.00' : amount
+      setBalance(balance)
+      const currency = currencyFinder(balanceData, userInfo, userCurrency)
+      setCurrency(currency)
+
+      const activeCurrency = userCurrency.currency.results.find((el) => el.abbreviation === currency)
+      setActiveCurrency(activeCurrency)
+    }
+  }, [userInfo, userCurrency])
 
   const showLinksMenuHandler = () => {
     setIsShowLinksMenu(true)
@@ -12,16 +51,31 @@ export const UserInformationBlock = ({ userInfo, userCurrency }) => {
   const hideLinksMenuHandler = () => {
     setIsShowLinksMenu(false)
   }
+  const closeDepositModalHandler = () => {
+    dispatch(showDepositModal(true))
+  }
 
   return userCurrency.currency && userInfo.balance ? (
     <div className={styles.userMainBlockUserInfoBlock}>
+      <div className={`${styles.userTextContainer}`}>
+        <span className={styles.userName}>
+          <div>
+            <div className={styles.balance}>{balance} <HeaderBalance currencyData={activeCurrency} /></div>
+            <div className={styles.depositButton} onClick={closeDepositModalHandler}>
+              {t('tournaments.buttons.deposit')}
+            </div>
+          </div>
+
+        </span>
+      </div>
       <div
         className={`${styles.userTextContainer} ${isShowLinksMenu ? styles.active : ''}`}
         onMouseEnter={() => showLinksMenuHandler()}
         onMouseLeave={() => hideLinksMenuHandler()}
       >
+
         <span className={styles.userName}>
-          {userInfo.user.user.username}
+          <img src="/assets/img/avatars/Blue.webp" width={50} alt="" />
         </span>
         {
           isShowLinksMenu
