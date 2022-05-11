103041ae6b021a888eb79a7f3d6f31d4a97ec4fa
Eric Huang <erichuang676@gmail.com>
2022-05-10 21:07:24 -0400

    refs:#1330 Contact Us redesign

--- components/HomePageComponents/WhySlotsIdol/WhySlotsIdol.js
+++ components/HomePageComponents/WhySlotsIdol/WhySlotsIdol.js
@@ -15,25 +15,25 @@ export const WhySlotsIdol = ({ isBackShow, title }) => {
       <div className={styles.whySlotsItems}>
         <div className={`${styles.minuteBlock} ${styles.whySlotsInnerItem}`}>
           <div className={styles.minuteImg}>
-            <img src={'/assets/img/whySlotsIdol/minute.webp'} alt="minute"/>
+            <img src={'/assets/img/whySlotsIdol/minute.svg'} alt="minute"/>
           </div>
           <p className={styles.minuteDescr}>{t(`whySlotsIdol.minuteRegistration`)}</p>
         </div>
         <div className={`${styles.depositsBlock} ${styles.whySlotsInnerItem}`}>
           <div className={styles.depositsImg}>
-            <img src={'/assets/img/whySlotsIdol/deposits.webp'} alt="deposits"/>
+            <img src={'/assets/img/whySlotsIdol/deposits.svg'} alt="deposits"/>
           </div>
           <p className={styles.depositsDescr}>{t(`whySlotsIdol.InstantDeposits`)}</p>
         </div>
         <div className={`${styles.payoutsBlock} ${styles.whySlotsInnerItem}`}>
           <div className={styles.payoutsImg}>
-            <img src={'/assets/img/whySlotsIdol/payouts.webp'} alt="payouts"/>
+            <img src={'/assets/img/whySlotsIdol/payouts.svg'} alt="payouts"/>
           </div>
           <p className={styles.payoutsDescr}>{t(`whySlotsIdol.FastPayouts`)}</p>
         </div>
         <div className={`${styles.licensedBlock} ${styles.whySlotsInnerItem}`}>
           <div className={styles.licensedImg}>
-            <img src={'/assets/img/whySlotsIdol/licensed.webp'} alt="licensed"/>
+            <img src={'/assets/img/whySlotsIdol/licensed.svg'} alt="licensed"/>
           </div>
           <p className={styles.licensedDescr}>{t(`whySlotsIdol.LicensedAndRegulated`)}</p>
         </div>
