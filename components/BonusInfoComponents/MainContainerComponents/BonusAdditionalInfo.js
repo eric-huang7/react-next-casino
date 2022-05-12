242b7b860c12bd6cb6e01d0a2f30f5c5ff397f8f
Eric Huang <erichuang676@gmail.com>
2022-05-11 23:51:07 -0400

    refs:#1333 Update bonus modal

--- components/BonusInfoComponents/MainContainerComponents/BonusAdditionalInfo.js
+++ components/BonusInfoComponents/MainContainerComponents/BonusAdditionalInfo.js
@@ -6,7 +6,7 @@ export const BonusAdditionalInfo = ({bonusData}) => {
   if (bonusData.min_deposit) {
     return (
       <div className={styles.additionalBonusInfo}>
-        <img src={'/assets/icons/arrowsIcon.png'} alt="icon exchange"/>
+        <img src={'/assets/icons/arrowsIcon-transparent.png'} alt="icon exchange"/>
         <p>
           {bonusData.min_deposit}
         </p>
