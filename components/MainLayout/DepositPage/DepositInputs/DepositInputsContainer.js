3ce3d3c1f4b85c4bb9b0559c1fc724e4e786c328
Eric Huang <erichuang676@gmail.com>
2022-05-16 17:33:08 -0400

    refs:#1236 Add currency icon

--- components/MainLayout/DepositPage/DepositInputs/DepositInputsContainer.js
+++ components/MainLayout/DepositPage/DepositInputs/DepositInputsContainer.js
@@ -1,8 +1,14 @@
 import styles from '../../../../styles/DepositPage/DepositPage.module.scss';
 import {DepositInputCount} from "./DepositInputCount";
 import ErrorText from "../../../ErrorBoundaryComponents/ErrorText";
+import {useEffect} from "react";
+import {svgSetterById} from "../../../../helpers/iconNameFinder";
 
 export const DepositInputsContainer = ({t, userCurrency, currencySwitcherShowHandler, userDepositValue, depositValueInputHandler, userDepositValueError}) => {
+  useEffect(() => {
+    const returnAbbr = false
+    svgSetterById(userCurrency?.userCurrencyData, `depositBalance${userCurrency?.userCurrencyData?.id}`, returnAbbr);
+  },[userCurrency?.userCurrencyData])
 
   return (
     <div className={styles.depositInputsBlock}>
@@ -20,6 +26,7 @@ export const DepositInputsContainer = ({t, userCurrency, currencySwitcherShowHan
           onClick={() => currencySwitcherShowHandler()}
           className={styles.depositCurrencyButton}
         >
+          <div id={`depositBalance${userCurrency?.userCurrencyData?.id}`} className={styles.iconContainer}></div>
           {userCurrency?.userCurrencyData?.abbreviation}
           <div className={styles.depositCurrencyButtonArrow}>
           </div>
