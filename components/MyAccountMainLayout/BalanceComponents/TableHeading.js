286be16f6fad891d14472a5f2ee94f8736f3540f
Eric Huang <erichuang676@gmail.com>
2022-05-16 22:32:46 -0400

    refs:#1242 Balance page sorting

--- components/MyAccountMainLayout/BalanceComponents/TableHeading.js
+++ components/MyAccountMainLayout/BalanceComponents/TableHeading.js
@@ -1,34 +1,27 @@
+import { FaSort } from "react-icons/fa";
+import { FaSortDown } from "react-icons/fa";
+import { FaSortUp } from "react-icons/fa";
 import styles from '../../../styles/MyAccount/BalancePage/BalancePage.module.scss';
 
 
-export const TableHeading = ({t}) => {
-
-
+export const TableHeading = ({onSort, columns, sort, direction}) => {
   return (
     <tr className={styles.headingRow} >
       <th className={styles.headingActive}>
 
       </th>
-      <th className={styles.headingCurrency}>
-        <p>
-          {t("myAccount.balance.table.headings.currency")}
-        </p>
-      </th>
-      <th className={styles.headingAmount}>
-        <p>
-          {t("myAccount.balance.table.headings.amount")}
-        </p>
-      </th>
-      <th className={styles.headingCashout}>
-        <p>
-          {t("myAccount.balance.table.headings.cashout")}
-        </p>
-      </th>
-      <th className={styles.headingActions}>
-        <p>
-          {t("myAccount.balance.table.headings.actions")}
-        </p>
-      </th>
+      {columns.map(column => (
+        <th
+          key={column.name}
+          className={column.style}
+          style={column.sort && {cursor: 'pointer'}}
+          onClick={() => column.sort && onSort(column.name)}
+        >
+          <p>
+            {column.title} {sort === column.name ? (direction ? <FaSortDown /> : <FaSortUp />) : column.sort && <FaSort color="#999" />}
+          </p>
+        </th>
+      ))}
     </tr>
   )
 }
