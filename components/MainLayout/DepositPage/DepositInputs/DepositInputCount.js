f7ee62ac02cf4eaee1ab6b1d2347e416bb866e88
Eric Huang <erichuang676@gmail.com>
2022-07-01 15:14:48 -0400

    refs:#1396 Styles update for deposit error message

--- components/MainLayout/DepositPage/DepositInputs/DepositInputCount.js
+++ components/MainLayout/DepositPage/DepositInputs/DepositInputCount.js
@@ -1,7 +1,13 @@
 import styles from '../../../../styles/DepositPage/DepositPage.module.scss';
+import {useDispatch} from "react-redux";
+import {setErrorUserDepositValue} from "../../../../redux/userFinance/action";
 
 export const DepositInputCount = ({t, currencySymbol, userDepositValue, depositValueInputHandler, userDepositValueError}) => {
+  const dispatch = useDispatch()
 
+  const onFocus = () => {
+    dispatch(setErrorUserDepositValue(''));
+  }
 
   return (
     <div className={styles.depositInputCountWrapper}>
@@ -10,9 +16,10 @@ export const DepositInputCount = ({t, currencySymbol, userDepositValue, depositV
       <input
         type="number"
         id="depositInputCount"
-        className={styles.depositSummInput}
+        className={`${styles.depositSummInput} ${userDepositValueError ? styles.errorMessage : ''}`}
         value={userDepositValue}
         onChange={(e) => depositValueInputHandler(e)}
+        onFocus={onFocus}
       />
       <span className={styles.errorMessage}>{userDepositValueError}</span>
     </div>
