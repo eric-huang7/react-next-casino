9876a6c373366804f189a0b0ffe0cb9f5ac946dd
Eric Huang <erichuang676@gmail.com>
2022-07-01 00:06:32 -0400

    refs:#1393 Update lint config and currency icons

---
+++ components/CurrencyIcon.js
@@ -0,0 +1,4 @@
+const CurrencyIcon = ({id, size = 24}) =>
+    <img src={`/assets/icons/currency/${id}.svg`} width={size} height={size} alt=""/>
+
+export default CurrencyIcon;
--- next.config.js
+++ next.config.js
@@ -5,5 +5,14 @@ module.exports = {
 
   images: {
     domains: ['cimagehost1.sos-ch-gva-2.exoscale-cdn.com']
-  }
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
