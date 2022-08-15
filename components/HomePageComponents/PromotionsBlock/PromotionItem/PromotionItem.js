5dfbf3b27569dbc7aa8a19dc7b6a00faa251a703
Eric Huang <erichuang676@gmail.com>
2022-08-15 13:13:36 -0400

    refs:#1393 Update Promotions block

--- components/HomePageComponents/PromotionsBlock/PromotionItem/PromotionItem.js
+++ components/HomePageComponents/PromotionsBlock/PromotionItem/PromotionItem.js
@@ -1,7 +1,8 @@
 import styles from "../../../../styles/HomePage/PromotionsBlock.module.scss";
 import {urlGen} from "../../../../helpers/imageUrl";
 import {useTranslation} from "next-i18next";
-
+import { Box, Text, VStack } from "@chakra-ui/react"
+import PrimaryButton from "../../../buttons/PrimaryButton";
 
 export const PromotionItem = ({bonusInfo, bonusCalculations}) => {
  const {t} = useTranslation("promotionsPage")
@@ -22,27 +23,44 @@ export const PromotionItem = ({bonusInfo, bonusCalculations}) => {
 
         </div>
         <div className={styles.promotionTextBlock}>
-          <div className={styles.frameTextBlock}>
-            <p className={styles.promotionMainText}>
+          <VStack
+            w="100%"
+            minH="254px"
+            h="100%"
+            borderBottom="1px solid #FFBF69"
+            borderLeft="1px solid #FFBF69"
+            borderRight="1px solid #FFBF69"
+            p="10px"
+          >
+            <Text
+              color="black"
+              // mb="10px"
+              fontSize="12px"
+              lineHeight="14px"
+              fontFamily="Verdana"
+              noOfLines={6}
+            >
               {t(`bonuses.bonus_${bonusInfo?.id}.description_long`, {x_key: bonusCalculations.x_key, y_key: bonusCalculations.y_key, wagner_require_key: bonusCalculations.wagner_require_key})}
-            </p>
-            <p className={styles.promotionAddText}>{bonusInfo.addText}</p>
-            <ul className={styles.promotionAddTextList}>
-              <li>
+            </Text>
+            <Text color="accent.800" textAlign="center">{bonusInfo.addText}</Text>
+            <Box fontSize="12px" lineHeight="21px" fontWeight={700} color="accent.800" textAlign="center" fontFamily="Verdana">
+              <Box>
                 {t(`bonuses.bonus_${bonusInfo?.id}.bonus_amount_info.max_bonus_amount`, {y_key: bonusCalculations.y_key})}
-              </li>
-              <li>
+              </Box>
+              <Box>
                 {t(`bonuses.bonus_${bonusInfo?.id}.bonus_amount_info.min_deposit_amount`, {min_deposit_key: bonusCalculations.min_deposit_key})}
-              </li>
-              <li>
+              </Box>
+              <Box>
                 {t(`bonuses.bonus_${bonusInfo?.id}.bonus_amount_info.playthrough_value`, {wagner_require_key: bonusCalculations.wagner_require_key})}
-              </li>
-            </ul>
-            <div style={{flex: 1}}></div>
-            <div className={styles.knowMoreBtn}>
-              <button>{t('bonuses.knowMore')}</button>
-            </div>
-          </div>
+              </Box>
+            </Box>
+            <div style={{flex: 1}} />
+            <Box w="100%" textAlign="center" pb="8px">
+              <PrimaryButton borderRadius={0} fontSize="13px" p="4px 25px" height="29px" width="auto">
+                {t('bonuses.knowMore')}
+              </PrimaryButton>
+            </Box>
+          </VStack>
         </div>
       </div>
     </div>
