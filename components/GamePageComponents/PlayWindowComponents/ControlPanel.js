52c4284219029fdf62715c2c8a255eb6e82843e4
Eric Huang <erichuang676@gmail.com>
2022-08-26 12:27:19 -0400

    refs:#1393 Refactor Game page components

--- components/GamePageComponents/PlayWindowComponents/ControlPanel.js
+++ components/GamePageComponents/PlayWindowComponents/ControlPanel.js
@@ -1,45 +1,11 @@
-import { Box, VStack } from "@chakra-ui/react"
+import { VStack } from "@chakra-ui/react"
+import {GameCloseButton, GameFullButton, GameMinimizeButton} from "../../buttons/GameControlButtons";
 
-const Button = ({children, ...props}) => (
-  <Box
-    backgroundColor="rgba(53, 69, 72, 0.5)"
-    w="24px"
-    h="24px"
-    minH="24px"
-    borderRadius="50%"
-    cursor="pointer"
-    transition="background-color 0.2s ease"
-    backgroundRepeat="no-repeat"
-    backgroundPosition="center"
-    backgroundSize="50%"
-    alignItems="center"
-    justifyContent="center"
-    _hover={{
-      backgroundColor: "rgba(83, 104, 110, 0.5)"
-    }}
-    {...props}
-  >
-    {children}
-  </Box>
-)
 export const ControlPanel = ({fullscreenClickHandler, closeGameHandler, minimizedHandler, isFullScreen}) => (
-  <VStack
-    w={isFullScreen ? "50px" : "30px"}
-    minW={isFullScreen ? "35px" : "25px"}
-    h="18%"
-    minH="100px"
-    bg="rgba(0,0,0, 0.5)"
-    position="absolute"
-    top={0}
-    right={isFullScreen ? 0: "-30px"}
-    alignItems="center"
-    p="5px"
-    spacing={2}
-  >
-    <Button onClick={closeGameHandler} backgroundImage="url('/assets/img/gamePage/close.png')" />
-    <Button onClick={fullscreenClickHandler} backgroundImage="url('/assets/img/gamePage/full.png')" />
-    <Button onClick={minimizedHandler} display="flex">
-      <Box w="10px" h="10px" borderTop="1px solid white" borderRight="1px solid white" transform="rotateZ(135deg)" mb="5px"/>
-    </Button>
+  <VStack h="18%" minH="100px" bg="rgba(0,0,0, 0.5)" position="absolute" top={0} alignItems="center" p="5px" spacing={2}
+          w={isFullScreen ? "50px" : "30px"} minW={isFullScreen ? "35px" : "25px"} right={isFullScreen ? 0: "-30px"}>
+    <GameCloseButton onClick={closeGameHandler} />
+    <GameFullButton onClick={fullscreenClickHandler} />
+    <GameMinimizeButton onClick={minimizedHandler} />
   </VStack>
 )
--- components/GamePageComponents/PlayWindowComponents/MinimizedControlPanel.js
+++ components/GamePageComponents/PlayWindowComponents/MinimizedControlPanel.js
@@ -1,11 +1,11 @@
-import styles from "../../../styles/GamePage/GamePage.module.scss";
+import { Text, HStack } from "@chakra-ui/react";
+import {GameCloseButton} from "../../buttons/GameControlButtons";
 
 export const MinimizedControlPanel = ({closeGameHandler, gameInfo}) => (
-  <div className={styles.minimizedControlPanel}>
-    <p>{`${gameInfo.gameName}`}</p>
-    <button
-      onClick={() => closeGameHandler()}
-      className={styles.closeButton}
-    />
-  </div>
+  <HStack alignItems="center" justifyContent="space-between" p="0 10px" h="20px" minH="20px" bg="#2b2b2b">
+    <Text m={0} fontSize="16px" color="#b1b3b4" fontFamily="Verdana" textAlign="center">
+      {`${gameInfo.gameName}`}
+    </Text>
+    <GameCloseButton onClick={closeGameHandler} />
+  </HStack>
 )
