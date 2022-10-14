d4b532fd60c6cb434d6a93b166474e1aaf41ca3d
Eric Huang <erichuang676@gmail.com>
2022-10-13 23:01:09 -0400

    refs:#1393 Refactor form input field

--- components/MyAccountMainLayout/TwoFactorAuth/GoogleAuthComponents/AuthCodeInputBlock.js
+++ components/MyAccountMainLayout/TwoFactorAuth/GoogleAuthComponents/AuthCodeInputBlock.js
@@ -2,8 +2,8 @@ import BodyText from "../../../typography/BodyText";
 import ErrorMessage from "../../../typography/ErrorMessage";
 import RoundButton from "../../../buttons/RoundButton";
 import Container from "../../ProfileInfoPage/ProfileInfoComponents/PhoneVerification/Container";
-import {Input} from "@chakra-ui/input";
 import {HStack} from "@chakra-ui/react";
+import InputFieldLight from "../../../form/InputFieldLight";
 
 export const AuthCodeInputBlock = ({
   t,
@@ -18,17 +18,7 @@ export const AuthCodeInputBlock = ({
     {googleAuthError && <ErrorMessage>{googleAuthError}</ErrorMessage>}
     <form onSubmit={confirmButtonClickHandler}>
       <HStack alignItems="center" justifyContent="space-between">
-        <Input onChange={(e) => googleKEyInputHandler(e.target.value)} value={googleKeyValue} type="text"
-               w="229px"
-               h="38px"
-               borderRadius="5px" bg="white"
-               border="none"
-               outline="none"
-               mr="10px"
-               fontSize="15px"
-               color="text.450"
-               fontFamily="Verdana"
-        />
+        <InputFieldLight onChange={googleKEyInputHandler} value={googleKeyValue} />
         <RoundButton type="submit" solid onClick={confirmButtonClickHandler}
           title={t('myAccount.twoFactorAuthPage.twoFaNOTCompleteContainer.codeInputBlock.confirmButton')} />
       </HStack>
--- components/MyAccountMainLayout/TwoFactorAuth/GoogleAuthComponents/TwoFactorCompleteInputsBlock.js
+++ components/MyAccountMainLayout/TwoFactorAuth/GoogleAuthComponents/TwoFactorCompleteInputsBlock.js
@@ -3,7 +3,6 @@ import ErrorMessage from "../../../typography/ErrorMessage";
 import Container from "../../../form/Container";
 import RoundButton from "../../../buttons/RoundButton";
 import {HStack} from "@chakra-ui/react";
-import {Input} from "@chakra-ui/input";
 
 export const TwoFactorCompleteInputsBlock = ({
   t,
@@ -13,22 +12,12 @@ export const TwoFactorCompleteInputsBlock = ({
   deactivateError
 }) => (
   <Container>
-    <BodyText bold fontSize={18}>{t('myAccount.twoFactorAuthPage.twoFaCompleteContainer.deactivateGoogleAuth')}</BodyText>
+    <BodyText bold fontSize={18}>222{t('myAccount.twoFactorAuthPage.twoFaCompleteContainer.deactivateGoogleAuth')}</BodyText>
     <BodyText pb={25}>{t('myAccount.twoFactorAuthPage.twoFaCompleteContainer.enterCurrentAuthCode')}</BodyText>
     {deactivateError && <ErrorMessage>{deactivateError}</ErrorMessage>}
     <form onSubmit={deactivateButtonClickHandler}>
       <HStack alignItems="center" justifyContent="space-between">
-        <Input onChange={(e) => deactivateInputHandler(e.target.value)} value={deactivateCodeValue} type="text"
-               w="229px"
-               h="38px"
-               borderRadius="5px" bg="white"
-               border="none"
-               outline="none"
-               mr="10px"
-               fontSize="15px"
-               color="text.450"
-               fontFamily="Verdana"
-        />
+        <InputFieldLight onChange={deactivateInputHandler} value={deactivateCodeValue} />
         <RoundButton type="submit" solid onClick={deactivateButtonClickHandler}
           title={t('myAccount.twoFactorAuthPage.twoFaCompleteContainer.confirmButton')} />
       </HStack>
