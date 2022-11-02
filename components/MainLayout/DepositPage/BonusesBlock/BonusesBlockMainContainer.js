ecfed4c63674ee222a2ee7f5fb8f5cf9e438d9e1
Eric Huang <erichuang676@gmail.com>
2022-11-02 19:03:01 -0400

    refs:#1393 Refactor Bonuses dropdown

--- components/MainLayout/DepositPage/BonusesBlock/BonusesBlockMainContainer.js
+++ components/MainLayout/DepositPage/BonusesBlock/BonusesBlockMainContainer.js
@@ -1,9 +1,8 @@
 import {useState} from "react";
-import {Box, HStack, VStack, Text} from "@chakra-ui/layout";
+import {Box, HStack, Text} from "@chakra-ui/layout";
 import {Input} from "@chakra-ui/input";
-import {ChevronDownIcon, ChevronUpIcon} from "@chakra-ui/icons"
 import CustomSwitch from "../../../form/CustomSwitch";
-import {BonusesDropdown} from "./BonusesDropdown";
+import BonusesDropdown from "./BonusesDropdown";
 
 export const BonusesBlockMainContainer = (props) => {
   let {
@@ -19,18 +18,6 @@ export const BonusesBlockMainContainer = (props) => {
     bonusesArr
   } = props
 
-  const [showAllBonuses, setShowAllBonuses] = useState(false)
-
-  const showAllBonusesHandler = (e) => {
-    e.stopPropagation()
-    e.preventDefault()
-    if (showAllBonuses) {
-      setShowAllBonuses(false)
-    } else {
-      setShowAllBonuses(true)
-    }
-  }
-
   return (
     <Box>
       <HStack justifyContent="space-between" pb={{base: "20px"}} spacing={0} alignItems="center">
@@ -40,19 +27,7 @@ export const BonusesBlockMainContainer = (props) => {
         <CustomSwitch isChecked={isChecked} onChange={checkedInputHandler}/>
       </HStack>
 
-      <HStack
-        h={showAllBonuses ? "auto" : "81px"}
-        w="100%"
-        bg="accent.700"
-        border="0"
-        borderRadius="12px"
-        alignItems="center"
-        justifyContent="space-between"
-        overflow="hidden"
-        spacing={0}
-        px="12px"
-      >
-        <BonusesDropdown
+      <BonusesDropdown
           showAllBonuses={showAllBonuses}
           userCurrency={userCurrency}
           t={t}
@@ -61,15 +36,8 @@ export const BonusesBlockMainContainer = (props) => {
           setDepositButtonText={setDepositButtonText}
           userDepositValue={userDepositValue}
           userSelectedBonus={userSelectedBonus}
-          allBonuses={bonusesArr}
-        />
-        {bonusesArr.length > 1 && <VStack h="60px" justifyContent="center">
-          {showAllBonuses
-            ? <ChevronUpIcon w={8} h={8} onClick={showAllBonusesHandler} color="primary.500" sx={{cursor: "pointer"}}/>
-            : <ChevronDownIcon w={8} h={8} onClick={showAllBonusesHandler} color="primary.500" sx={{cursor: "pointer"}}/>
-          }
-        </VStack>}
-      </HStack>
+          allBonuses={[...bonusesArr, ...bonusesArr, ...bonusesArr]}
+      />
 
       {isActiveBonusInput && <Box mt={2} w="100%">
         <Input
--- components/MainLayout/DepositPage/BonusesBlock/BonusesDropdown.js
+++ components/MainLayout/DepositPage/BonusesBlock/BonusesDropdown.js
@@ -3,32 +3,104 @@ import {iconsUrl} from "../../../../helpers/imageUrl";
 import {bonusInfoCalculator} from "../../../../helpers/bonusInfoCalculator";
 import ErrorText from "../../../ErrorBoundaryComponents/ErrorText";
 import {Box} from "@chakra-ui/react";
+import {useEffect, useState} from "react";
+import {HStack, VStack} from "@chakra-ui/layout";
+import {ChevronDownIcon, ChevronUpIcon} from "@chakra-ui/icons";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from '@chakra-ui/react'
+import {Button} from "@chakra-ui/button";
 
 const iDontNeedBonus = {id: 1, heading: "bonuses.bonusBlockInfoNotBonus", info: "", icon: '/assets/icons/stop.png'};
 
-export const BonusesDropdown = ({
+const Item = ({ children, ...props}) => <MenuItem
+  px="0"
+  w="calc(100% - 10px)"
+  _hover={{backgroundColor: 'transparent'}}
+  _active={{backgroundColor: 'transparent'}}
+  _focus={{backgroundColor: 'transparent'}}
+  bg={`transparent url('/assets/img/currency/line.svg') repeat-x center`}
+  {...props}
+>{children}</MenuItem>
+
+const BonusesDropdown = ({
   t, allBonuses, isUseBonus,
   chooseBonusClickHandler, userSelectedBonus, userCurrency
 }) => {
-  const isEmpty = !(allBonuses?.length && isUseBonus);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [selected, setSelected] = useState();
 
-  if (isEmpty) {
-    chooseBonusClickHandler(0)
+  useEffect(() => {
+    setIsEmpty(!(allBonuses?.length && isUseBonus));
+    if (allBonuses?.length) {
+      setSelected(allBonuses[0]);
+    }
+  }, [allBonuses?.length])
+
+  useEffect(() => {
+    if (isEmpty) {
+      chooseBonusClickHandler(0);
+    }
+  }, [isEmpty])
+
+  const onChange = (value) => {
+    // TODO
+    setSelected(value)
   }
 
-  return isEmpty
-    ? <BonusesBlock
-      t={t}
-      classImageNotActive={'imageNotActive'}
-      bonusImage={iDontNeedBonus.icon}
-      bonusHeading={iDontNeedBonus.heading}
-      bonusDescription={iDontNeedBonus.info}
-      isUseBonus={isUseBonus}
-      bonusLink={'/#bonusLink'}
-      canShowInfo={false}
-    />
-    : <Box flex={1}>
-      {allBonuses.sort((el) => {
+  const EmptyItem = () => <BonusesBlock
+    t={t}
+    classImageNotActive={'imageNotActive'}
+    bonusImage={iDontNeedBonus.icon}
+    bonusHeading={iDontNeedBonus.heading}
+    bonusDescription={iDontNeedBonus.info}
+    isUseBonus={isUseBonus}
+    bonusLink={'/#bonusLink'}
+    canShowInfo={false}
+  />
+
+  const bonusDescription = selected && bonusInfoCalculator(selected, userCurrency.userCurrencyData, t);
+
+  return <Menu onChange={onChange} closeOnSelect>
+    <MenuButton
+      as={Button}
+      rightIcon={allBonuses?.length > 1 && <ChevronDownIcon w={8} h={8} color="white" />}
+      h="81px"
+      w="100%"
+      bg="accent.700"
+      border="0"
+      borderRadius="12px"
+      alignItems="center"
+      justifyContent="space-between"
+      _hover={{bg: 'accent.700'}}
+      _focus={{bg: 'accent.700'}}
+      _active={{bg: 'accent.700'}}
+      overflow="hidden"
+      spacing={0}
+      px="12px"
+    >
+      {isEmpty
+        ? <EmptyItem/>
+        :  selected && <BonusesBlock
+          t={t}
+          bonusId={selected.id}
+          bonusImage={iconsUrl(selected.icon)}
+          bonusHeading={`bonuses.bonus_${selected.id}.deposit_bonus.heading`}
+          bonusDescription={`${bonusDescription.max_bonus}${bonusDescription.free_spins_amount ? ` + ${bonusDescription.free_spins_amount} ${t('bonusInfoContainer.bonusInfo.freeSpins')}` : ''}`}
+          isUseBonus={isUseBonus}
+          chooseBonusClickHandler={chooseBonusClickHandler}
+          bonusData={selected}
+          userCurrency={userCurrency}
+          canShowInfo={true}
+        />
+      }
+    </MenuButton>
+    <MenuList minW="460px" bg="accent.700" borderRadius={12} border="none" p="10px">
+      {isEmpty && <Item><EmptyItem /></Item>}
+      {!isEmpty && allBonuses.sort((el) => {
         let res = el.id === userSelectedBonus.bonus_id ? -1 : 1
         if (userSelectedBonus.bonus_id === 0) {
           res = el.id === 1 ? -1 : 1
@@ -38,7 +110,7 @@ export const BonusesDropdown = ({
 
         let bonusDescription = bonusInfoCalculator(el, userCurrency.userCurrencyData, t);
 
-        return (
+        return (<Item key={`${el.id} bonus`}>
           <ErrorText key={`${el.id} bonus`}>
             <BonusesBlock
               key={`${el.id} bonus`}
@@ -54,7 +126,10 @@ export const BonusesDropdown = ({
               canShowInfo={true}
             />
           </ErrorText>
-        )
+        </Item>)
       })}
-    </Box>
+    </MenuList>
+  </Menu>
 }
+
+export default BonusesDropdown;
