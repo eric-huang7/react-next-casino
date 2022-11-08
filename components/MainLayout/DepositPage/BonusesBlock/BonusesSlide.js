2ef2823ec877e0690d7d2be1869c086cde99ef96
Eric Huang <erichuang676@gmail.com>
2022-11-07 22:51:42 -0500

    refs:#1393 Design Bonuses slider

--- components/MainLayout/DepositPage/BonusesBlock/BonusesSlide.js
+++ components/MainLayout/DepositPage/BonusesBlock/BonusesSlide.js
@@ -1,41 +1,58 @@
 import {useTranslation} from "next-i18next";
 import { Text, HStack, Box, Image } from "@chakra-ui/react";
 import {VStack} from "@chakra-ui/layout";
+import CurrencyIcon from "../../../currency/CurrencyIcon";
 
-const BonusesSlide = (props) => {
+const BonusesSlide = ({amount = '', abbr = 'BTC', active = fals, onClick}) => {
   const {t} = useTranslation('promotionsPage');
 
   return (
-    <VStack
-      w="220px"
-      h="201px"
-      px="0"
-      py="20px"
-      cursor="pointer"
-      spacing={0}
-      borderRadius="12px"
-      alignItems="center"
-      justifyContent="center"
-      border="4px solid"
-      borderColor="#097E2E"
-      bg="url('assets/img/mainLayoutImg/background.webp')"
-
-    >
-      <HStack height="30px" justifyContent="center" alignItems="center" width="calc(100% - 40px)" bg="#AD4B1D" borderRadius="12px"
+    <Box px="10px" w="240px">
+      <VStack
+        w="220px"
+        h="201px"
+        px="0"
+        pt="23px"
+        pb="19px"
+        cursor="pointer"
+        spacing={0}
+        borderRadius="12px"
+        alignItems="center"
+        justifyContent="center"
+        position="relative"
+        bg="url('assets/img/mainLayoutImg/background.webp')"
+        onClick={() => onClick && onClick()}
       >
-        <img src="/assets/icons/deposit/gift.svg" width="22px" height="22px" alt="" />
-        <Text fontSize={16} fontWeight={500} color="white" fontFamily="Montserrat">
-          50 Free Spains
-        </Text>
-      </HStack>
-      <Text pt="11px" pb="9px" px="20px" fontSize={17} fontWeight={500} color="white" fontFamily="Montserrat" textTransform="capitalize">
-        Min Deposit: $20.00
-      </Text>
+        {active && <Box position="absolute" top={0} right={0} w="220px" h="201px" border="4px solid"
+            borderColor="#097E2E" borderRadius="12px" />}
+        {active && <Box position="absolute" top={0} right={0} w="23px" h="23px" bg="#097E2E" borderBottomLeftRadius="6px"
+             borderTopRightRadius="12px" display="flex" alignItems="center" justifyContent="center">
+          <img src="/assets/icons/deposit/check-small.svg" width="12px" height="8px" />
+        </Box>}
 
-      <HStack mt="15px" height="44px" justifyContent="center" alignItems="center" width="calc(100% - 30px)" bg="#AD4B1D" borderRadius="12px" mx="15px">
-        <Text px="20px" fontSize={24} fontWeight={500} color="white" fontFamily="Montserrat">1:20:35:5</Text>
-      </HStack>
-    </VStack>
+        <HStack minH="30px" justifyContent="center" alignItems="center" w="calc(100% - 50px)" bg="#AD4B1D"
+          borderRadius="100px"
+        >
+          <img src="/assets/icons/deposit/gift.svg" width="22px" height="22px" alt="" />
+          <Text fontSize={16} fontWeight={500} color="white" fontFamily="Montserrat">
+            50 Free Spains
+          </Text>
+        </HStack>
+        <Text pt="9px" pb="7px" px="20px" fontSize={17} fontWeight={400} color="white" fontFamily="Montserrat" textTransform="capitalize">
+          Min Deposit: $20.00
+        </Text>
+        <HStack justifyContent="center" alignItems="center" pb="12px">
+          <CurrencyIcon id={abbr} size={8}/>
+          <Text fontSize={18} fontWeight={500} color="white" fontFamily="Montserrat">{amount} {abbr}</Text>
+        </HStack>
+        <HStack minH="44px" justifyContent="center" alignItems="center" w="calc(100% - 30px)" bg="#AD4B1D"
+          borderRadius="12px" mx="15px">
+          <Text px="20px" fontSize={24} fontWeight={500} color="white" fontFamily="Montserrat" letterSpacing="0.25em">
+            1:20:35:5
+          </Text>
+        </HStack>
+      </VStack>
+    </Box>
   )
 }
 
--- components/MainLayout/DepositPage/BonusesBlock/BonusesSlider.js
+++ components/MainLayout/DepositPage/BonusesBlock/BonusesSlider.js
@@ -2,9 +2,74 @@ import {useTranslation} from "next-i18next";
 import { Text, HStack, Box, Image } from "@chakra-ui/react";
 import {VStack} from "@chakra-ui/layout";
 import BonusesSlide from "./BonusesSlide";
+import Slider from "react-slick";
+import BonusArrowButton from "../../../buttons/BonusArrowButton";
+import {useState} from "react";
 
 const BonusesSlider = (props) => {
   const {t} = useTranslation('promotionsPage');
+  const [active, setActive] = useState()
+  const slides = [
+    {
+      bonus: '50 Free Spains',
+      amount: '0.0256832',
+      abbr: 'BTC',
+      time: '1:20:35:5',
+    },
+    {
+      bonus: '50 Free Spains',
+      amount: '0.0256832',
+      abbr: 'BTC',
+      time: '1:20:35:5',
+    },
+    {
+      bonus: '50 Free Spains',
+      amount: '0.0256832',
+      abbr: 'BTC',
+      time: '1:20:35:5',
+    }
+  ];
+
+
+  function SampleNextArrow(props) {
+    const { currentSlide, onClick } = props;
+
+    return currentSlide + 1 < slides?.length ? (
+      <BonusArrowButton
+        onClick={onClick}
+        direction="next"
+        round
+        w="120px"
+      />
+    ) : null;
+  };
+
+  function SamplePrevArrow(props) {
+    const { onClick, currentSlide } = props;
+
+    return currentSlide > 0 ? (
+      <BonusArrowButton
+        onClick={onClick}
+        direction="prev"
+        display
+        round
+        w="121px"
+      />
+    ) : null;
+  };
+
+  const sliderSettings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    rows: 1,
+    slidesPerRow: 1,
+    centerMode: true,
+    centerPadding: '130px',
+    nextArrow: <SampleNextArrow/>,
+    prevArrow: <SamplePrevArrow/>
+  }
 
   return (
     <VStack
@@ -19,9 +84,21 @@ const BonusesSlider = (props) => {
       <Text px="20px" fontSize={17} fontWeight={300} color="white" fontFamily="Montserrat" textTransform="capitalize">
         Choose a bonus
       </Text>
-      <HStack h={270} justifyContent="center" alignItems="center" width="100%">
-        <BonusesSlide />
-      </HStack>
+      <Box w="100%" maxW="500px" h={270} m="0 auto" position="relative">
+        <Slider {...sliderSettings}>
+          {slides?.map((item, index) => (
+            <BonusesSlide
+              key={index}
+              amount={item.amount}
+              bonus={item.bonus}
+              abbr={item.currency}
+              time={item.time}
+              active={active === index}
+              onClick={() => setActive(index)}
+            />
+          ))}
+        </Slider>
+      </Box>
     </VStack>
   )
 }
